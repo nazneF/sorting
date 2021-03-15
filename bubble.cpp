3ff041c85406317ccf70e1cc94b98fedd9022b60
NAZNE <naznefernandes@gmail.com>
2021-03-15 11:59:44 +0530

    ccp file

---
+++ bubble.cpp
@@ -0,0 +1,32 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,n,temp;
+	printf("Enter the number of elements:");
+	scanf("%d",&n);		//to take the input oh how many elements are there in total
+	int a[n];
+	printf("enter the elements: ");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);		//to take the elements given by the user
+	}
+	printf("the sorted elements in the array are:");
+	for(i=1;i<n-1;i++)
+	{
+		for(j=0;j<n-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+				temp=a[j];
+				a[j]=a[j+1];
+				a[j+1]=temp; 
+			}
+		}
+	}
+	
+	for(i=0;i<n;i++)
+	{
+		printf("%d  ",a[i]);		//statement to print the sorted elements
+	}
+	return 0;
+}
